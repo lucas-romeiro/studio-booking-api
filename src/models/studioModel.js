811465f30332da264c5323d50a45538b489df336
Lucas Romeiro <lucas_romeiro@outlook.com>
2025-05-13 12:13:55 -0300

    refactor(schema): altera nomenclatura de studio para estúdio

--- src/models/studioModel.js
+++ src/models/studioModel.js
@@ -4,33 +4,33 @@ const studioSchema = new mongoose.Schema(
   {
     nome: {
       type: String,
-      required: [true, 'Um studio deve possuir um nome'],
+      required: [true, 'Um estúdio deve possuir um nome'],
       unique: true,
       trim: true,
-      maxlength: [20, 'O nome do studio deve conter até 20 caracteres'],
-      minlength: [5, 'O nome do studio deve conter pelo menos 5 caracteres'],
+      maxlength: [20, 'O nome do estúdio deve conter até 20 caracteres'],
+      minlength: [5, 'O nome do estúdio deve conter pelo menos 5 caracteres'],
       validate: {
         validator: function (value) {
           return /^[A-Za-zÀ-ÿ\s]+$/.test(value);
         },
-        message: 'O nome do studio deve conter apenas letras',
+        message: 'O nome do estúdio deve conter apenas letras',
       },
     },
     localizacao: {
       type: String,
-      required: [true, 'Um studio deve possuir uma localização'],
+      required: [true, 'Um estúdio deve possuir uma localização'],
       trim: true,
-      maxlength: [30, 'A localização do studio deve conter até 30 caracteres'],
+      maxlength: [30, 'A localização do estúdio deve conter até 30 caracteres'],
       minlength: [
         10,
-        'A localização do studio deve conter pelo menos 10 caracteres',
+        'A localização do estúdio deve conter pelo menos 10 caracteres',
       ],
     },
     capacidade: {
       type: Number,
-      required: [true, 'Um studio deve ter uma capacidade máxima'],
-      min: [5, 'Um studio deve suportar no mínimo 5 pessoas'],
-      max: [20, 'Um studio deve suportar no máximo 20 pessoas'],
+      required: [true, 'Um estúdio deve ter uma capacidade máxima'],
+      min: [5, 'Um estúdio deve suportar no mínimo 5 pessoas'],
+      max: [20, 'Um estúdio deve suportar no máximo 20 pessoas'],
     },
   },
   { timestamps: true }
