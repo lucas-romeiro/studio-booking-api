875f7e98522fd175665341d7c1b8914dadae0ddd
Lucas Romeiro <lucas_romeiro@outlook.com>
2026-06-01 17:47:19 -0300

    chore(app): initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Studio Booking API')
+    .setDescription(
+      'A REST API for booking studio rooms for rehearsals and recording sessions',
+    )
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
