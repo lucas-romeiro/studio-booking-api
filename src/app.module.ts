817226042ddb67d54ebffb7ca7709d92d014865f
Lucas Romeiro <lucas_romeiro@outlook.com>
2026-06-05 15:49:49 -0300

    feat(config): add user and database modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './infrastructure/config';
+import { DatabaseModule } from './infrastructure/database';
+import { UserModule } from './interface/modules';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, UserModule, DatabaseModule],
 })
 export class AppModule {}
