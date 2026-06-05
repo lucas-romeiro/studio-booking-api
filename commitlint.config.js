f0eca6e19d4420b796388d7e16385d315e649c74
Lucas Romeiro <lucas_romeiro@outlook.com>
2026-06-05 15:34:13 -0300

    chore(config): updates scope validation rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'config',
+        'database',
+        'user',
+        'auth',
+        'room',
+        'booking',
+        'payment',
+        'review',
+        'queue',
+        'test',
+      ],
+    ],
+  },
 };
