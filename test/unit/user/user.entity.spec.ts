ade3417fc714f3a71455ec73bc3587e8b7cd7d41
Lucas Romeiro <lucas_romeiro@outlook.com>
2026-06-05 15:55:22 -0300

    test(user): add user entity unit tests

---
+++ test/unit/user/user.entity.spec.ts
@@ -0,0 +1,33 @@
+import { DomainError } from '@/domain/shared/errors';
+import { Email, User, UserRole } from '@/domain/user';
+
+describe('User entity', () => {
+  const validProps = {
+    name: 'John Doe',
+    email: new Email('john_doe@email.com'),
+    passwordHash: 'hash1234',
+  };
+
+  it('should create a user with default role musician', () => {
+    const user = User.create(validProps);
+    expect(user.role).toBe(UserRole.MUSICIAN);
+    expect(user.id).toBeDefined();
+  });
+
+  it('should throw an error if name is less than 2 characters', () => {
+    expect(() => User.create({ ...validProps, name: 'J' })).toThrow(
+      DomainError,
+    );
+  });
+
+  it('should promote an user to admin', () => {
+    const user = User.create(validProps);
+    user.promoteToAdmin();
+    expect(user.role).toBe(UserRole.ADMIN);
+  });
+
+  it('should throw an error to promote admin twice', () => {
+    const user = User.create({ ...validProps, role: UserRole.ADMIN });
+    expect(() => user.promoteToAdmin()).toThrow(DomainError);
+  });
+});
