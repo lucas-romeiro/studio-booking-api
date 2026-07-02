cfe101e1eb0f306cc9f0e9a347634d9d03f315a0
Lucas Romeiro <lucas_romeiro@outlook.com>
2026-07-02 07:24:23 -0300

    feat(auth): add roles guard and roles decorator

---
+++ src/interface/http/guards/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '@/domain/user';
+import { ROLES_KEY } from '../decorators';
+import { TokenPayload } from '@/domain/auth';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { user } = context
+      .switchToHttp()
+      .getRequest<{ user: TokenPayload }>();
+
+    if (!requiredRoles.includes(user.role as UserRole)) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    return true;
+  }
+}
