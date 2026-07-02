fd9332adfc32a3feca30baecc8e208e246e8255d
Lucas Romeiro <lucas_romeiro@outlook.com>
2026-07-02 07:44:24 -0300

    chore(config): update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './infrastructure/config';
 import { DatabaseModule } from './infrastructure/database';
-import { UserModule } from './interface/modules';
+import { AuthModule, UserModule } from './interface/modules';
+import { QueueModule } from './infrastructure/queue';
 
 @Module({
-  imports: [AppConfigModule, UserModule, DatabaseModule],
+  imports: [
+    AppConfigModule,
+    UserModule,
+    DatabaseModule,
+    AuthModule,
+    QueueModule,
+  ],
 })
 export class AppModule {}
